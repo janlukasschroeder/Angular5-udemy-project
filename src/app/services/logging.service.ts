c7cf2e0d8633c6baeaf900fe7b210990132398fe
janlukasschroeder <jan@constructioncloud.com.au>
2018-04-10 07:57:14 +1000

    Added basic logging service used by shopping-edit component

---
+++ src/app/services/logging.service.ts
@@ -0,0 +1,5 @@
+export class LoggingService {
+  log(msg) {
+    console.log('Log: ' + msg);
+  }
+}
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,17 +1,19 @@
 import {Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
 import {Ingredient} from "../../shared/ingredient.model";
+import {LoggingService} from "../../services/logging.service";
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
+  providers: [LoggingService]
 })
 export class ShoppingEditComponent implements OnInit {
   @ViewChild('nameIngredient') nameIngredientRef: ElementRef;
   @ViewChild('amountIngredient') amountIngredientRef: ElementRef;
   @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
-  constructor() { }
+  constructor(private logger: LoggingService) { }
 
   ngOnInit() {
   }
@@ -21,6 +23,7 @@ export class ShoppingEditComponent implements OnInit {
     const amountIngredient = this.amountIngredientRef.nativeElement.value;
     const newIngredient = new Ingredient(nameIngredient, amountIngredient);
     this.ingredientAdded.emit(newIngredient);
+    this.logger.log('New ingredient added');
   }
 
 }
