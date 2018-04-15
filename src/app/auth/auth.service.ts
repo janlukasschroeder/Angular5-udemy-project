2ca5f69720f01ed8954d61bc13964091c133a026
janlukasschroeder <janlukasschroeder@protonmail.com>
2018-04-15 12:22:22 +1000

    Added auth token support in fetch/save queries

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import * as firebase from 'firebase';
 
 export class AuthService {
+  token = '';
+
   signupUser(email: string, password: string) {
     firebase.auth().createUserWithEmailAndPassword(email, password)
       .catch(
@@ -10,7 +12,14 @@ export class AuthService {
 
   signinUser(email: string, password: string) {
     firebase.auth().signInWithEmailAndPassword(email, password)
-      .then(response => console.log(response))
+      .then(() => {
+        firebase.auth().currentUser.getToken().then(tk => this.token = tk);
+      })
       .catch(error => console.log(error));
+  }
+
+  getToken() {
+    firebase.auth().currentUser.getToken().then(tk => this.token = tk);
+    return this.token;
   }
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -3,19 +3,26 @@ import {HttpClient, HttpResponse} from '@angular/common/http';
 
 import {RecipeService} from '../recipes/recipe.service';
 import {Recipe} from '../recipes/recipe.model';
+import {AuthService} from '../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
 
   constructor(private http: HttpClient,
-              private recipeService: RecipeService) {}
+              private recipeService: RecipeService,
+              private authService: AuthService) {}
 
   storeRecipes () {
-    return this.http.put('https://ng-recipe-book-623ba.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+    const token = this.authService.getToken();
+
+    return this.http.put('https://ng-recipe-book-623ba.firebaseio.com/recipes.json?auth=' + token,
+      this.recipeService.getRecipes());
   }
 
   getRecipes() {
-    this.http.get('https://ng-recipe-book-623ba.firebaseio.com/recipes.json')
+    const token = this.authService.getToken();
+
+    this.http.get('https://ng-recipe-book-623ba.firebaseio.com/recipes.json?auth=' + token)
       .subscribe((response: Recipe[]) => {
         const recipes: Recipe[] = response;
         this.recipeService.updateRecipes(recipes);
